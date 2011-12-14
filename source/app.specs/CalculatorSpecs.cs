f1474ed276b6588bd9d3d73892331bfee3fc4491
codingstorm <email.raza@gmail.com>
2011-12-14 03:15:14 +0000

    Pushing new changes

---
+++ source/app.specs/CalculatorSpecs.cs
@@ -0,0 +1,12 @@
+﻿namespace app.specs
+{
+    using Machine.Specifications;
+
+    public class CalculatorSpecs
+    {
+        public class when_calculator_is_asked_to_add
+        {
+            It should_return_the_sum = () => Calculator.add(2, 3).ShouldEqual(5);
+        }
+    }
+}
---
+++ source/app/Calculator.cs
@@ -0,0 +1,11 @@
+namespace app.specs
+{
+    public class Calculator
+
+    {
+        public static int add(int a, int b)
+        {
+            return a + b; 
+        }
+    }
+}
