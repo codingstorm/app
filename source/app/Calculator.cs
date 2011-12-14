62155e4cdbd8f433404a08b24b99ac1789ac1ef3
codingstorm <email.raza@gmail.com>
2011-12-14 03:24:35 +0000

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,14 @@
 ﻿namespace app
 {
-  public class Calculator
+    using System;
+
+    public class Calculator
   {
     public int add(int first, int second)
     {
+        if(first < 0 || second < 0)
+            throw new ArgumentException();
+
         return first + second;
     }
   }
