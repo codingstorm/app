536204436e4194e6eb9bfbddfe74d2a3b7efab2f
codingstorm <email.raza@gmail.com>
2011-12-16 20:22:54 +0000

    Pushing new changes

--- source/app/tasks/startup/StartupChainBuilder.cs
+++ source/app/tasks/startup/StartupChainBuilder.cs
@@ -1,7 +1,18 @@
 ﻿namespace app.tasks.startup
 {
-  public class StartupChainBuilder : ICreateStartupChains
-  {
-     
-  }
+    using System;
+    using System.Collections.Generic;
+
+    public class StartupChainBuilder : ICreateStartupChains
+    {
+        IList<Type> all_types;
+        Type type;
+
+        public StartupChainBuilder(IList<Type> allTypes, Type type)
+        {
+            all_types = allTypes;
+            this.type = type;
+            all_types.Add(this.type);
+        }
+    }
 }
