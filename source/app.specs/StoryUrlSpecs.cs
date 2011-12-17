aa77955fb7a37de9f4b2ef878bc2332fe1c259a5
ballance <ballance@gmail.com>
2011-12-16 20:53:20 -0500

    Pushing new changes

---
+++ source/app.specs/StoryUrlSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using app.infrastructure.containers;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+
+namespace app.specs
+{
+    [Subject(typeof(StoryUrlSpecs))]
+    public class StoryUrlSpecs
+    { 
+        public abstract class concern : Observes
+        {
+        }
+
+        public class when_doing_something_with_a_story_url : concern
+        {
+            Establish c = () =>
+            {
+
+            };
+
+            Because b = () =>
+            {
+
+            };
+
+            It should_do_something_in_particular = () =>
+            {
+
+            };
+        }
+    }
+}
